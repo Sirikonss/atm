435df2b40f47c2cbb9cfbbb8867961a7ef817b4e
Sirikonss <s.sirikonss@gmail.com>
2020-09-11 15:58:59 +0700

    add form and refactor code

---
+++ src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -0,0 +1,26 @@
+package th.ac.ku.atm.controller;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    private List<Customer> customerList;
+
+    @PostConstruct
+    public void postConstruct() {
+        this.customerList = new ArrayList<>();
+    }
+
+    public void createCustomer(Customer customer) {
+        customerList.add(customer);
+    }
+
+    public List<Customer> getCustomers() {
+        return new ArrayList<>(this.customerList);
+    }
+}
