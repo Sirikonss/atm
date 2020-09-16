d9d325bca3517ea1de9d0dcd054887703e5756c4
Sirikonss <s.sirikonss@gmail.com>
2020-09-16 12:51:41 +0700

    add bootstrap, layout, hash, and login

--- src/main/java/th/ac/ku/atm/controller/CustomerController.java
+++ src/main/java/th/ac/ku/atm/controller/CustomerController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
+import th.ac.ku.atm.model.Customer;
+import th.ac.ku.atm.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
--- src/main/java/th/ac/ku/atm/service/CustomerService.java
+++ src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -1,6 +1,8 @@
-package th.ac.ku.atm.controller;
+package th.ac.ku.atm.service;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
+import th.ac.ku.atm.model.Customer;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -9,7 +11,7 @@ import java.util.List;
 @Service
 public class CustomerService {
 
-    private List<Customer> customerList;
+    private ArrayList<Customer> customerList;
 
     @PostConstruct
     public void postConstruct() {
@@ -17,10 +19,42 @@ public class CustomerService {
     }
 
     public void createCustomer(Customer customer) {
+        // .... hash pin ....
+        String hashPin = hash(customer.getPin());
+        customer.setPin(hashPin);
         customerList.add(customer);
     }
 
     public List<Customer> getCustomers() {
         return new ArrayList<>(this.customerList);
+    }
+
+    public Customer findCustomer(int id) {
+        for (Customer customer : customerList) {
+            if (customer.getId() == id)
+                return customer;
+        }
+        return null;
+    }
+
+    public Customer checkPin(Customer inputCustomer) {
+        // 1. หา customer ที่มี id ตรงกับพารามิเตอร์
+        Customer storedCustomer = findCustomer(inputCustomer.getId());
+
+        // 2. ถ้ามี id ตรง ให้เช็ค pin ว่าตรงกันไหม โดยใช้ฟังก์ชันเกี่ยวกับ hash
+        if (storedCustomer != null) {
+            String hashPin = storedCustomer.getPin();
+
+            if (BCrypt.checkpw(inputCustomer.getPin(), hashPin))
+                return storedCustomer;
+        }
+        // 3. ถ้าไม่ตรง ต้องคืนค่า null
+        return null;
+    }
+
+
+    private String hash(String pin) {
+        String salt = BCrypt.gensalt(12);
+        return BCrypt.hashpw(pin, salt);
     }
 }
